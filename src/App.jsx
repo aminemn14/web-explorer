48ab562bb3540a5425862b8985deb77366af5236
Mathys Dezitter <mathysdezitter.pro@gmail.com>
2024-12-02 12:18:41 +0100

    Feature - create function to get a random number at loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+
+
 function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 6 + 1));
+
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 6 + 1);
+  }
+  
+  useEffect(() => {
+    setNumber(randomNumber());
+    console.log(number)
+  }, []);
+
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Web Explorer</h1>
